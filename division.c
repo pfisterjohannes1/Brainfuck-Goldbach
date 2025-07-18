04754ec7fd8d26f0f5571416d32fd5dd7c05ee4d
Johannes Pfister <pfisterjohannes@protonmail.com>
2025-07-18 23:29:01 +0200

    Made complete goldbach in C

--- division.c
+++ division.c
@@ -16,95 +16,94 @@ f tests if a integer >3 is the sum of 2 primes
 
 
 
-void f(int N)
+int main(void)
 {
-  //Test if N is sumnof 2 primes
 
-  int s1=2;
-  int s2=N-2;
-  int found=0;
-  while(s2-1)
+  int found=1;
+  int N=2;
+  while(found)
     {
-      int bothPrime=0;
-      int testS2=2;
-      while(testS2)
+      N++;
+      N++;
+      //Test if N is sumnof 2 primes
+      int s1=2;
+      int s2=N-2;
+      found=0;
+      while(s2-1) //we decrease second sumand and increase first till second one is 1
         {
-          testS2--;
-          int prime=s1;
-          if(testS2)
+          int bothPrime=0; //increased for every summand which is prime
+          int testS2=2; //Which summand we test and how many loops (2).
+          while(testS2) //test both summands for beeing prime
             {
-              prime=s2;
-            }
-          int r=0;
-          int t=0;
-          int c=1; //possible diviser we Currently test for
-          int bb[]={0,1,0}; //placeholder, we later need it to implement if in brainfuck
-          int a=0;
-          int searching = 1;
-          while(searching)
-            {
-              c++;
-              r=1; //Running
-              t=0;
-              bb[0]=prime;
-              a=c;
-              while(r)
+              testS2--;
+              int prime=s1; //whicch number we test for beeing prime
+              if(testS2)
                 {
-                  t=a;
-                  while(t)
-                    {
-                      r--;
-                      t--;
-                      bb[0]--;
-                      if(bb[0])
-                        { r++; }
-                    }
+                  prime=s2;
                 }
-              searching--;
-              if(bb[0])
-                { searching++; }
+              int r=0;
+              int t=0;
+              int c=1; //we test if this is a divisor.
+              int bb[]={0,1,0}; //placeholder, we later need it to implement if in brainfuck
+              int a=0;
+              int searching = 1;
+              while(searching) //search for the smallest divisor >1
+                {
+                  c++;
+                  r=1; //Running
+                  t=0;
+                  bb[0]=prime; //copy of prime we can decrease.
+                  a=c;
+                  while(r) //we calculate prime%c or bb%a
+                    {
+                      t=a;
+                      while(t)
+                        {
+                          r--;
+                          t--;
+                          bb[0]--;
+                          if(bb[0])
+                            { r++; }
+                        }
+                    }
+                  searching--;
+                  if(bb[0]) //set when c was not a divisor of prime (prime%c!=0)
+                    { searching++; }
+                }
+              //test if c==prime, if this is true the smallest divisor is prime
+              // and prime is actually a prime number
+              while(prime)
+                {
+                  prime--;
+                  c--;
+                }
+              bothPrime++;
+              if( c ) //true if c!=prime => not a prime number
+                {
+                  bothPrime--;
+                }
             }
-          while(prime)
-            {
-              prime--;
-              c--;
-            }
-          bothPrime++;
-          if( c )
-            {
-              bothPrime--;
-            }
-        }
 
-      bothPrime--;
-      bothPrime--;
-      found++;
-      if( bothPrime )
-        {
-          found--;
+          found++;
+
+          //found a pair if bothPrime is 2 (both summands are prime)
+          bothPrime--;
+          bothPrime--;
+          if( bothPrime )
+            {
+              found--;
+            }
+          s2--; //test next summand pair
+          s1++;
         }
-      s2--;
-      s1++;
+      printf("found N %i, %i\n",N,found);
     }
-  //found has the number of how many pairs we found that are both prime
-  printf("found %i %i\n",found,N);
 }
 
 
 
 
 
-int main(int argc, char **argv)
-  {
-    int a,b;
-    (void)argc;
-    (void)argv;
-    for(b=4;b<200;b++)
-      {
-        f(b);
-      }
-    return 0;
-  }
 
 
 
